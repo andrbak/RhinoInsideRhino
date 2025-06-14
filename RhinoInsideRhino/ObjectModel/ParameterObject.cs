0481d175a9c2377f76aec37988d5b8725fb1cfcd
Andreas Bak <116800100+andrbak@users.noreply.github.com>
2025-06-14 20:40:03 +0200

    Adde ParameterObject

---
+++ RhinoInsideRhino/ObjectModel/ParameterObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhinoInsideRhino.ObjectModel
+{
+    class ParameterObject
+    {
+
+        public string Type { get; set; }
+
+        public object Value { get; set; }   
+    }
+}
