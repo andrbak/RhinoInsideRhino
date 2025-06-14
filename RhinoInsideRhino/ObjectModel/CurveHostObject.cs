77036925bd9084c653064c255c7d69cb3723a720
XGar <xavier.garnavault@gmail.com>
2025-06-14 22:21:08 +0200

    Update request + initial objectproperties page

--- RhinoInsideRhino/ObjectModel/CurveHostObject.cs
+++ RhinoInsideRhino/ObjectModel/CurveHostObject.cs
@@ -3,9 +3,13 @@ using Rhino.Display;
 using Rhino.DocObjects.Custom;
 using Rhino.Geometry;
 using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using Newtonsoft;
+using System.IO;
+using System.IO.Compression;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -101,9 +105,84 @@ namespace RhinoInsideRhino.ObjectModel
 
         public void Update()
         {
-            //Call Compute here!
+            RhinoApp.WriteLine("Object Changed");
+            // Serialize the data to JSON
+            var inputsJson = new Dictionary<string, object>
+            {
+                ["txt_in"] = Compress(Geometry.ToJSON(new Rhino.FileIO.SerializationOptions()))
+            };
+            foreach (KeyValuePair<string, ParameterObject> parameter in Data.Parameters)
+            {
+                inputsJson[parameter.Key] = parameter.Value.Value;
+            }
 
-            RhinoApp.WriteLine($"Object ({Id}) has been updated!");
+            var requestBody = new Dictionary<string, object>
+            {
+                ["inputs"] = inputsJson,
+                ["model"] = new Dictionary<string, string>
+                {
+                    ["model"] = Data.ModelId.ToString(),
+                    ["type"] = "GH"
+                }
+            };
+            // Construct the HTTP POST request
+
+            RhinoApp.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(requestBody).ToString());
+            var request = (HttpWebRequest)WebRequest.Create("https://api.prod.configurator-backend.modelup3d.com/compute?outputId=" + Data.outputId);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.UserAgent = "AecTech25Hack";
+            request.Headers.Add("Authorization", $"Bearer {Data.token}");
+            request.Headers.Add("x-compute-server", "wilfred-r8");
+            byte[] byteArray = Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(requestBody));
+            request.ContentLength = byteArray.Length;
+            // Send the request to Modelup
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            string output = string.Empty;
+            // Read and display the server response
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string txt_out = reader.ReadToEnd();
+                    output = txt_out;
+                }
         }
+
+            // Decompress the response
+            string decompressedOutput = Decompress(output);
+            // Deserialize the response
+            var outputData = Rhino.Geometry.GeometryBase.FromJSON(decompressedOutput);
+        }
+        public static string Compress(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return text;
+        byte[] inputBytes = Encoding.UTF8.GetBytes(text);
+            var output = new MemoryStream();
+        // leaveOpen=true lets us read the MemoryStream after disposing the GZipStream
+        using (var gzip = new GZipStream(output, CompressionLevel.Optimal, leaveOpen: true))
+        {
+            gzip.Write(inputBytes, 0, inputBytes.Length);
+        }
+        return Convert.ToBase64String(output.ToArray());
+    }
+        
+        public static string Decompress(string base64)
+        {
+            if (string.IsNullOrEmpty(base64))
+                return base64;
+            byte[] compressedBytes = Convert.FromBase64String(base64);
+            var input = new MemoryStream(compressedBytes);
+            var gzip = new GZipStream(input, CompressionMode.Decompress);
+            var output = new MemoryStream();
+            gzip.CopyTo(output);
+            return Encoding.UTF8.GetString(output.ToArray());
+        }
+
     }
 }
--- RhinoInsideRhino/RhinoInsideRhinoPlugin.cs
+++ RhinoInsideRhino/RhinoInsideRhinoPlugin.cs
@@ -40,5 +40,11 @@ namespace RhinoInsideRhino
         // You can override methods here to change the plug-in behavior on
         // loading and shut down, add options pages to the Rhino _Option command
         // and maintain plug-in wide options in a document.
+
+        protected override void ObjectPropertiesPages(List<ObjectPropertiesPage> pages)
+        {
+            var page = new Views.HostObjectPropertiesPage();
+            pages.Add(page);
+        }
     }
 }
